f7806d2eca7da536df7ede9b27e5a41a534a1dcc
IrinaBolgova <irinabolgova047@gmail.com>
2025-11-13 14:28:34 +0500

    final?

---
+++ 1_Calculator/1_Calculator/Migrations/20251113092806_1234567897.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _1_Calculator.Migrations
+{
+    /// <inheritdoc />
+    public partial class _1234567897 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
