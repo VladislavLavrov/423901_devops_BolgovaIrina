151ecacdacee8190496d4470ee4ca2b02da6170a
IrinaBolgova <irinabolgova047@gmail.com>
2025-12-12 13:04:47 +0500

    Изменение кода

--- 1_Calculator/1_Calculator/Program.cs
+++ 1_Calculator/1_Calculator/Program.cs
@@ -1,8 +1,31 @@
+using OpenTelemetry.Metrics;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(meterProviderBuilder =>
+    {
+        meterProviderBuilder.AddPrometheusExporter();
 
+
+        meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Hosting",
+                                      "Microsoft.AspNetCore.Server.Kestrel");
+
+     
+        meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Http.Connections");
+
+       
+        meterProviderBuilder.AddView("http.server.request.duration",
+            new ExplicitBucketHistogramConfiguration
+            {
+                Boundaries = new double[]
+                {
+                    0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10
+                }
+            });
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
